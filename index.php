0ed91a120577964a0a46d61c851afcdaf0dca0b5
Francisco Garcia <ciscog11@msn.com>
2016-01-13 19:24:58 -0700

    new simple html

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Francisco Garcia</title>
+		<link href='https://fonts.googleapis.com/css?family=Monofett' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<h1>Me</h1>
+		<h1 class="titlefont"></h1>
+	</body>
+</html>
